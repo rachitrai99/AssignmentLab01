123bb32603e4d93f4a3d05e76793be70108f16c0
kshitij879 <kshitij.gupta_cs18@gla.ac.in>
2020-04-20 21:02:01 +0530

    done

---
+++ src/Solution.java
@@ -0,0 +1,40 @@
+package main;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 1, 2, 7};
+        occurance(arr, 2);
+        duplicates(arr);
+    }
+
+    public static void occurance(int[] arr, int element) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == element) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void duplicates(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        boolean dup = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+        System.out.println("duplicate values are:");
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 1) {
+                System.out.print(entry.getKey() + " ");
+            }
+        }
+
+    }
+}
